b7cb6bb16873ce9240ec9aad2cbe07c283b4406c
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-26 15:20:26 -0300

    "Seção 9: App Finanças Pessoais - 89. Creando Projeto - Update"

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,33 @@
+/**
+ *  Installation React Navigaion - Navigators - NativeStack
+ *    npm install @react-navigation/native-stack
+ *  
+ * Installation React Navigaion - Navigators - Drawer
+ *    npm install @react-navigation/drawer
+*/
+
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+const AuthStack = createNativeStackNavigator();
+
+function AuthRoutes() {
+    return (
+        <AuthStack.Navigator>
+            <AuthStack.Screen
+                name="SignIn"
+                component={SignIn}
+            />
+
+            <AuthStack.Screen
+                name="SignUp"
+                component={SignUp}
+            />
+        </AuthStack.Navigator>
+    );
+}
+
+
+export default AuthRoutes;
