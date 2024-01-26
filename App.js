e351e9ec3e6023b52ec86eac01b51520b26ddb4d
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-26 11:46:44 -0300

    Seção 9: App Finanças Pessoais - 89. Creando Projeto - bibliotecas necessaria https://reactnavigation.org/docs/getting-started

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+/** 
+ *  Installation
+ *    npm install @react-navigation/native
+ *    npm install react-native-screens react-native-safe-area-context
+ * 
+ *  Installing dependencies into an Expo managed project
+ *    npx expo install react-native-screens react-native-safe-area-context
+ * 
+ */
+
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
