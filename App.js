dd4fcfb1b72cad0c9ed178974ad1760930505417
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-26 15:19:46 -0300

    Seção 9: App Finanças Pessoais - 89. Creando Projeto - Update

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
-/** 
- *  Installation
- *    npm install @react-navigation/native
- *    npm install react-native-screens react-native-safe-area-context
- * 
- *  Installing dependencies into an Expo managed project
- *    npx expo install react-native-screens react-native-safe-area-context
- * 
- */
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+
+    <NavigationContainer>
+      <StatusBar backgroundColor={'#F0F4FF'} barStyle={'dark-content'} />
+      <Routes />      
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
